19ac1c3ddc3d21bca6a94740d3dea74957c2c867
hushenshen <1245613916@qq.com>
2021-01-21 10:31:34 +0800

    file📦 深拷贝

---
+++ 深拷贝.js
@@ -0,0 +1,43 @@
+const depClone = (cloneObj) => {
+  if (typeof cloneObj !== 'object') {
+    return cloneObj
+  }
+  // Array instanceof Object -> true
+  let copyObj = cloneObj instanceof Array ? [] : {}
+
+  for (let key in cloneObj) {
+    let _obj = cloneObj[key]
+
+    switch (typeof _obj) {
+      case 'object':
+        copyObj[key] = depClone(_obj)
+        break
+      case 'function':
+        copyObj[key] = _obj.bind(copyObj)
+        break
+      default:
+        copyObj[key] = _obj
+        break
+    }
+  }
+  return copyObj
+}
+
+let obj1 = {
+  a: 1,
+  b: {
+    c: 2
+  },
+  d() {
+    console.log(this.b.c)
+  }
+}
+
+let cloneObj1 = depClone(obj1)
+
+obj1.b.c = 3
+
+console.log(obj1, cloneObj1)
+
+obj1.d()
+cloneObj1.d()
